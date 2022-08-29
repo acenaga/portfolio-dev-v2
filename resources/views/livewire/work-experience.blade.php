f8271af13b260796ed1703dd85e1c7086c9a4a32
Carlos Ferrer <Carlos.Alberto@Sci-us.com>
2022-08-29 16:33:47 -0300

    update and linter

--- resources/views/livewire/work-experience.blade.php
+++ resources/views/livewire/work-experience.blade.php
@@ -2,14 +2,14 @@
     <!-- Worked Enterprises-->
     <div class="mh-work-item dark-bg wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.4s">
         @foreach ($experiences as $experience)
-            
+
         @endforeach
         <h4>{{ $experience->position }} <a href="{{ $experience->url }}">{{ $experience->company_name }}</a></h4>
         <div class="mh-eduyear">{{ $experience->start_date }}-{{ $experience->end_date }}</div>
         <span>Responsibility :</span>
         <ul class="work-responsibility">
             @foreach ($experience->responsa as $item)
-                
+
             @endforeach
             <li><i class="fa fa-circle"></i>Validate CSS</li>
             <li><i class="fa fa-circle"></i>Project Management</li>
