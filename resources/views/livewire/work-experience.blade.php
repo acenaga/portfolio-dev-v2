089231090114b7b456dbc52046af85c12ae02075
Carlos Ferrer <mundofuturoca@gmail.com>
2024-02-14 20:59:01 -0300

    added validation in work experience and fixed format in front

--- resources/views/livewire/work-experience.blade.php
+++ resources/views/livewire/work-experience.blade.php
@@ -2,8 +2,8 @@
     <!-- Worked Enterprises-->
     <div class="mh-work-item dark-bg wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.4s">
         @foreach ($experiences as $experience)
-            <h4>{{ $experience->position }} <a href="{{ $experience->url }}">{{ $experience->company_name }}</a></h4>
-            <div class="mh-eduyear">{{ $experience->start_date }}-{{ $experience->end_date }}</div>
+            <h4>{{ $experience->position }} // <a href="{{ $experience->url }}">{{ $experience->company_name }}</a></h4>
+            <div class="mh-eduyear">{{ $experience->start_date }} -- {{ $experience->end_date }}</div>
             <span>Responsibility :</span>
             <ul class="work-responsibility">
                 @foreach ($experience->responsibilities as $responsibility)
